ad807f68a77744dd743644e379461a09f052ff84
Foxwhite <belgeomax@gmail.com>
2015-08-16 18:02:17 +0300

    add stop/start, loading gif

--- index.js
+++ index.js
@@ -14,53 +14,89 @@ if (contextClass) {
 // Create the source.
 var source = context.createBufferSource();
 // Create the gain node.
-var gain = context.createGain();
+// var gain = context.createGain();
 // Connect source to filter, filter to destination.
-source.connect(gain);
-gain.connect(context.destination);
+// source.connect(gain);
+source.connect(context.destination);
+
+var startTime = 0;
+var file = null;
+var audioBuffer = null;
 
 $('.upload_btn').on('change', function(e){
-    var file = e.target.files[0];
+    file = e.target.files[0];
     $('.uploaded_file').html(file.name);
+    $('.stopstart').removeClass('nowplaying');
     readFile(file);
 });
 
+
+$('.stopstart').on('click', function(){
+    if (!$(this).hasClass('disabled')){
+
+        $(this).toggleClass('nowplaying');
+        if ($(this).hasClass('nowplaying')){
+            $(this).html('Stop');
+            if (file) playSound(audioBuffer);    
+        }
+        else {
+            $(this).html('Play');    
+            if (file) stopSound();    
+
+        }
+    }
+});
 
 
 var handleDragOver = function(e) {
     e.preventDefault()
     e.stopPropagation()
+    $('.dz-text').html('Drop music files here');
 }
 
 var handleDrop = function(e) {
     e.preventDefault()
     e.stopPropagation()
 
-    var file = e.dataTransfer.files[0];
+    file = e.dataTransfer.files[0];
     $('.uploaded_file').html(file.name);
-    //readFile(file);
+    readFile(file);
 }
 
 function readFile(file){
+    $('.dz-text').html('<div class="loading"></div>');
+
     var reader = new FileReader()
     reader.addEventListener('load', function(e) {
         var data = e.target.result
         context.decodeAudioData(data, function(buffer) {
-                playSound(buffer)
+                audioBuffer = buffer;
+                $('.stopstart').removeClass('disabled');
+                $('.dz-text').html('File loaded!');
             })
         })
     reader.readAsArrayBuffer(file)
 }
 
 function playSound(buffer) {
-  var source = context.createBufferSource();
-  source.buffer = buffer;
-  source.connect(context.destination);
-  source.start(0);
+    // startTime = context.currentTime;
+    source = context.createBufferSource();
+    source.buffer = buffer;
+    source.connect(context.destination);
+    source.start(0);
 }
 
 function stopSound() {
-  if (source) {
-    source.stop();
-  }
+    if (source) {
+        source.stop();
+    }
 }
+
+function pauseSound() {
+    if (source) {
+        source.stop(0);
+        source = null;
+        var position = context.currentTime - startTime;
+        this.playing = false;
+    }
+};
