24ea24edf7d75ef38a4d7d60435ba29266e0d0e6
Foxwhite <belgeomax@gmail.com>
2015-08-16 21:20:05 +0300

    visualizer

--- index.js
+++ index.js
@@ -1,53 +1,3 @@
-
-var contextClass = (window.AudioContext || 
-  window.webkitAudioContext || 
-  window.mozAudioContext || 
-  window.oAudioContext || 
-  window.msAudioContext);
-if (contextClass) {
-  // Web Audio API is available.
-  var context = new contextClass();
-} else {
-    $('.dz-text').css('color', '#D00202').html('Your browser does not support Web Audio Api. Sorry');    
-}
-
-// Create the source.
-var source = context.createBufferSource();
-// Create the gain node.
-// var gain = context.createGain();
-// Connect source to filter, filter to destination.
-// source.connect(gain);
-source.connect(context.destination);
-
-var startTime = 0;
-var file = null;
-var audioBuffer = null;
-
-$('.upload_btn').on('change', function(e){
-    file = e.target.files[0];
-    $('.uploaded_file').html(file.name);
-    $('.stopstart').removeClass('nowplaying');
-    readFile(file);
-});
-
-
-$('.stopstart').on('click', function(){
-    if (!$(this).hasClass('disabled')){
-
-        $(this).toggleClass('nowplaying');
-        if ($(this).hasClass('nowplaying')){
-            $(this).html('Stop');
-            if (file) playSound(audioBuffer);    
-        }
-        else {
-            $(this).html('Play');    
-            if (file) stopSound();    
-
-        }
-    }
-});
-
-
 var handleDragOver = function(e) {
     e.preventDefault()
     e.stopPropagation()
@@ -62,6 +12,62 @@ var handleDrop = function(e) {
     $('.uploaded_file').html(file.name);
     readFile(file);
 }
+
+    
+
+var contextClass = (window.AudioContext || 
+  window.webkitAudioContext || 
+  window.mozAudioContext || 
+  window.oAudioContext || 
+  window.msAudioContext);
+
+if (contextClass) {
+  // Web Audio API is available.
+  var context = new contextClass();
+} 
+else {
+    $('.dz-text').css('color', '#D00202').html('Your browser does not support Web Audio Api. Sorry');    
+}
+
+
+
+var source = context.createBufferSource();
+    // source.connect(context.destination);
+    if (!source.start) {
+        source.start = source.noteOn //in old browsers use noteOn method
+        source.stop = source.noteOff //in old browsers use noteOff method
+    };
+
+
+var file = null;
+var audioBuffer = null;
+var animation = null;
+var nowplaying = false;
+
+
+$('.upload_btn').on('change', function(e){
+    file = e.target.files[0];
+    $('.uploaded_file').html(file.name);
+    $('.stopstart').removeClass('nowplaying');
+    readFile(file);
+});
+
+$('.stopstart').on('click', function(){
+    if (!$(this).hasClass('disabled')){
+
+        if (!nowplaying){
+            $(this).html('Stop');
+            if (file) playSound(audioBuffer);    
+        }
+        else {
+            $(this).html('Play');    
+            if (file) stopSound();    
+
+        }
+    }
+});
+
+
 
 function readFile(file){
     $('.dz-text').html('<div class="loading"></div>');
@@ -79,24 +85,59 @@ function readFile(file){
 }
 
 function playSound(buffer) {
-    // startTime = context.currentTime;
     source = context.createBufferSource();
     source.buffer = buffer;
-    source.connect(context.destination);
+
+    var analyser = context.createAnalyser();
+        // analyser.smoothingTimeConstant = 0.3;
+        // analyser.fftSize = 1024;
+
+    source.connect(analyser);
+    analyser.connect(context.destination);
+
     source.start(0);
+    nowplaying = true;
+    drawSpectrum(analyser);
 }
 
 function stopSound() {
+    nowplaying = false;
     if (source) {
         source.stop();
     }
+
 }
 
-function pauseSound() {
-    if (source) {
-        source.stop(0);
-        source = null;
-        var position = context.currentTime - startTime;
-        this.playing = false;
+var drawSpectrum = function (analyser){
+    $('.dz-text').html('');
+    var ctx = $("#player_canvas").get(0).getContext("2d");
+        
+    var gradient = ctx.createLinearGradient(0,0,0,300);
+        gradient.addColorStop(1,'#001E47');
+        gradient.addColorStop(0.75,'#044FB6');
+        gradient.addColorStop(0.25,'#5E9DF2');
+        gradient.addColorStop(0,'#ffffff');
+    
+    ctx.fillStyle=gradient;
+
+    var drawer = function(){
+        var array = new Uint8Array(analyser.frequencyBinCount);
+            analyser.getByteFrequencyData(array);
+
+        var step = Math.round(array.length / 120);
+        ctx.clearRect(0, 0, 400, 300);
+        
+        
+        for ( var i = 0; i < 120; i++ ){
+            var value = array[i * step];
+            ctx.fillRect(i*5,200-value,3,200);
+        }
+
+        if (!nowplaying)  {
+            cancelAnimationFrame(animation);
+            ctx.clearRect(0, 0, 400, 300);
+        }
+        else {animation = requestAnimationFrame(drawer);}
     }
-};
+    animation = requestAnimationFrame(drawer);
+}
